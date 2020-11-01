f8b26f025838502d886de2b7f1c210e770fc150f
Vinícius Margotti <viniciusmargotti@hotmail.com>
2020-10-31 21:38:21 -0300

    Adicionando dependencias no package.json

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express();
+app.use (express.static ('./ dist / <name-on-package.json> '));
+
+app.get ('/ *', function (req, res) {
+  res.sendFile ('index.html', {root: 'dist / <name-on-package.json> /'}
+  );
+});
+
+app.listen(process.env.PORT || 8080);
